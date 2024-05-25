7570b6fc75fb9175ab440580f1d6f35bb088e0f2
rylenko <rylenko@tuta.io>
2024-05-26 02:39:07 +0700

    Add sending chain struct

--- crates/shield/src/chain/send.rs
+++ crates/shield/src/chain/send.rs
@@ -1,1 +1,28 @@
+/// Sending chain of Double Ratchet algorithm.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub(crate) struct Send {
+	key: Option<crate::key::Master>,
+	header_key: Option<crate::key::Header>,
+	next_header_key: crate::key::Header,
+	next_msg_num: u32,
+	prev_msgs_cnt: u32,
+}
 
+impl Send {
+	/// Creates new sending chain using passed keys.
+	#[inline]
+	#[must_use]
+	pub(crate) const fn new(
+		key: Option<crate::key::Master>,
+		header_key: Option<crate::key::Header>,
+		next_header_key: crate::key::Header,
+	) -> Self {
+		Self {
+			key,
+			header_key,
+			next_header_key,
+			next_msg_num: 0,
+			prev_msgs_cnt: 0,
+		}
+	}
+}
