4ba59b2d78f48c1ec564052d38f951439d674364
rylenko <rylenko@tuta.io>
2024-05-30 01:02:45 +0700

    Add sending chain upgrading method

--- crates/shield/src/chain/send.rs
+++ crates/shield/src/chain/send.rs
@@ -65,10 +65,37 @@ impl Send {
 	pub(crate) fn forward(
 		&mut self,
 	) -> Result<crate::key::Message, ForwardError> {
-		match self.key {
-			Some(ref mut key) => Ok(key.evolve()),
-			None => Err(ForwardError::NoKey),
-		}
+		// Try to retrieve current master key and evolve it to get message key.
+		let message_key =
+			self.key.as_mut().ok_or(ForwardError::NoKey)?.evolve();
+
+		// Increase next message number because new message key evolved.
+		self.next_message_num += 1;
+		Ok(message_key)
+	}
+
+	/// Upgrades the chain with a new master [`key`] and a new next [header
+	/// key]. In other words, it is as if a new chain is created with
+	/// information about the previous chain.
+	///
+	/// The current next header key becomes the current header key, the number
+	/// of messages in the chain is preserved, and the message numbering is
+	/// reset to zero.
+	///
+	/// [`key`]: crate::key::Master
+	/// [header key]: crate::key::Header
+	pub(crate) fn upgrade(
+		&mut self,
+		key: crate::key::Master,
+		next_header_key: crate::key::Header,
+	) {
+		self.key = Some(key);
+		self.header_key = Some(core::mem::replace(
+			&mut self.next_header_key,
+			next_header_key,
+		));
+		self.prev_chain_messages_count = self.next_message_num;
+		self.next_message_num = 0;
 	}
 }
 
@@ -81,10 +108,40 @@ mod tests {
 			Some(crate::key::Header::new([1; 32])),
 			crate::key::Header::new([2; 32]),
 		);
+
+		// Test success of forward moving.
 		assert!(chain.forward().is_ok());
 		assert_ne!(chain.key, Some(crate::key::Master::new([0; 32])));
+		assert_eq!(chain.next_message_num, 1);
 
+		// Test forward moving with no master key.
 		chain.key = None;
 		assert_eq!(chain.forward(), Err(super::ForwardError::NoKey));
+	}
+
+	#[test]
+	fn test_upgrade() -> Result<(), super::ForwardError> {
+		let mut chain = super::Send::new(
+			Some(crate::key::Master::new([0; 32])),
+			Some(crate::key::Header::new([1; 32])),
+			crate::key::Header::new([2; 32]),
+		);
+
+		// Move chain forward.
+		for _ in 0..10 {
+			chain.forward()?;
+		}
+
+		// Test upgrading method.
+		chain.upgrade(
+			crate::key::Master::new([10; 32]),
+			crate::key::Header::new([11; 32]),
+		);
+		assert_eq!(chain.key, Some(crate::key::Master::new([10; 32])));
+		assert_eq!(chain.header_key, Some(crate::key::Header::new([2; 32])));
+		assert_eq!(chain.next_header_key, crate::key::Header::new([11; 32]));
+		assert_eq!(chain.next_message_num, 0);
+		assert_eq!(chain.prev_chain_messages_count, 10);
+		Ok(())
 	}
 }
